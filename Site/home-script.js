d4edbf211b2efecae52a512eabc49c75b9166fe0
mattheus971 <mathvelozo2000@gmail.com>
2025-05-26 00:18:00 -0300

    Fiz algumas alterações

---
+++ Site/home-script.js
@@ -0,0 +1,6 @@
+// function inserirLetraUsuario() {
+//     const usuarioInput = document.getElementById('usuario');
+//     const usuarioInicial = document.getElementById('usuario-inicial');
+//     const primeiraLetra = usuarioInput.value.charAt(0).toUpperCase();
+//     usuarioInicial.textContent = primeiraLetra;
+// }
--- Site/login-logon-script.js
+++ Site/login-logon-script.js
@@ -1,24 +1,36 @@
-function cadastrarUsuario() {
-    const usuario = document.getElementById('usuario').value;
-    const email = document.getElementById('email').value;
-    const senha = document.getElementById('senha').value;
-    const telefone = document.getElementById('telefone').value;
+function mostrarLogin() {
+    document.getElementById('formulario-login').style.display = "flex"
+    document.getElementById('formulario-cadastro').style.display = "none"
 
-    if (usuario && senha && email && telefone) {
-        alert(`Cadastro realizado! Bem-vindo, ${usuario}`)
-        alert(usuario + "\n" + senha + "\n" + email + "\n" + telefone + "\n")
+}
+
+function mostrarCadastro() {
+    document.getElementById('formulario-cadastro').style.display = "flex"
+    document.getElementById('formulario-login').style.display = "none"
+}
+
+function cadastrarUsuario() {
+    let usuario = document.getElementById('usuario').value
+    let email = document.getElementById('email-cadastro').value
+    let senha = document.getElementById('senha-cadastro').value
+    let telefone = document.getElementById('telefone').value
+
+    if (usuario && email && senha && telefone) {
+        alert(` Usuario: ${usuario}\n Email: ${email}\n Senha: ${senha}\n Telefone: ${telefone}\n`)
     } else {
-        alert("Por favor, preencha todos os campos.");
+        alert("Por favor, preencha todos os campos")
     }
 }
 
 function logar() {
-    const email = document.getElementById('email').value;
-    const senha = document.getElementById('senha').value;
+    let email = document.getElementById('email').value
+    let senha = document.getElementById('senha').value
 
     if (email && senha) {
-        alert(email + "\n" + senha)
+        alert(` Email: ${email}\n Senha: ${senha}\n`)
     } else {
-        alert("Por favor, preencha todos os campos.");
+        alert("Por favor, preencha todos os campos")
     }
 }
+
+// Login e Cadastro com os mesmos id's
