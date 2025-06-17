3b02b383de9ad1f287daab5d4260fc8f67dd9f13
mattheus971 <mathvelozo2000@gmail.com>
2025-06-16 22:09:34 -0300

    Comecei as funções cadastro/login

--- Site/cadastro-login-script.js
+++ Site/cadastro-login-script.js
@@ -1,12 +1,14 @@
-function inicializar(){
+function inicializar() {
     document.getElementById('inputEmailLogin').focus()
 }
 inicializar()
 
-function esconderTudo(){
+
+function esconderTudo() {
     document.getElementById('formulario-cadastro').style.display = "none"
     document.getElementById('formulario-login').style.display = "none"
 }
+
 
 function mostrarLogin() {
     esconderTudo()
@@ -14,16 +16,50 @@ function mostrarLogin() {
     document.getElementById('inputEmailLogin').focus()
 }
 
+
 function mostrarCadastro() {
     esconderTudo()
     document.getElementById('formulario-cadastro').style.display = "flex"
     document.getElementById('inputNomeCadastro').focus()
 }
 
-// function cadastrarUsuario() {
-// }
+const usuarios = []
+function cadastrarUsuario() {
+    const nome = document.getElementById('inputNomeCadastro').value
+    const email = document.getElementById('inputEmailCadastro').value
+    const senha = document.getElementById('inputSenhaCadastro').value
 
-// function logar() {
-// }
+    if (!nome || !email || !senha) {
+        alert("Por favor, preencha todos os campos.");
+        return;
+    } else {
+        const usuario = {
+            nome: nome,
+            email: email,
+            senha: senha
+        };
+        usuarios.push(usuario);
+        alert("Usuário cadastrado com sucesso!");
+        mostrarLogin()
+    }
+    document.getElementById('inputNomeCadastro').value = "";
+    document.getElementById('inputEmailCadastro').value = "";
+    document.getElementById('inputSenhaCadastro').value = "";
+}
+
+function logar() {
+    const emailLogin = document.getElementById('inputEmailLogin').value;
+    const senhaLogin = document.getElementById('inputSenhaLogin').value;
+
+    const usuarioEncontrado = usuarios.find(u => u.email === emailLogin && u.senha === senhaLogin);
+
+    if (usuarioEncontrado) {
+        window.location.href = "home.html";
+    } else {
+        alert("Usuário ou senha incorreto");
+    }
+    document.getElementById('inputEmailLogin').value = "";
+    document.getElementById('inputSenhaLogin').value = "";
+}
 
 
